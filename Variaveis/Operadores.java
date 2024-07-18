880fc60f0287f88a9c5eeb46d007c5b23136d61e
maria.emoliveira <elianamotadeoliveira@gmail.com>
2024-07-17 22:51:15 -0300

    atualizações

---
+++ Variaveis/Operadores.java
@@ -0,0 +1,30 @@
+package Variaveis;
+
+public class Operadores {
+
+public static void main(String args []){
+
+  int numero = 5;
+    
+  numero += 12;
+
+  System.out.println(numero);
+
+  int x = 20;
+
+  x = ++x +5;
+
+  System.out.println(x);
+
+  int y = 20;
+
+  y = y++ +5;
+
+  System.out.println(y);
+
+}
+
+}
+
+
+
